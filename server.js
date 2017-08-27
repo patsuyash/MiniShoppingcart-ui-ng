c5ab0234ad3dbe1115b7a4f886e702721c9f5c50
Suyash Patil <patsuyash@gmail.com>
2017-08-27 17:33:40 +0530

    te

--- server.js
+++ server.js
@@ -16,3 +16,10 @@ app.set('port', (process.env.PORT || 8180));
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+app.use(function(req, res, next) {
+    if(req.protocol !== 'https') {
+        return res.status(403).send({message: 'SSL required'});
+    }
+    // allow the request to continue
+    next();
+});
